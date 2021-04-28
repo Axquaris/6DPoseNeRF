6600339b9cd1315f3efa4e583e40cda6bb182975
axquaris <dominykas@berkeley.edu>
2021-04-27 20:38:14 -0700

    optimization loop visualization

--- inerf/optimization_loop.py
+++ inerf/optimization_loop.py
@@ -1,4 +1,5 @@
 import torch
+import torchshow as ts
 import svox
 import matplotlib.pyplot as plt
 import kornia
@@ -24,12 +25,15 @@ class iNeRF(torch.nn.Module):
         self.NeRF_renderer = NeRF_Renderer(self.image_res)
         self.svox_renderer = SVOX_Renderer(self.image_res)
 
-        init_c2w = self.NeRF_renderer.dataset[1]["c2w"]
+        init_c2w = self.NeRF_renderer.dataset[7]["c2w"]
         # Use euler angles, yaw and pitch as parameters of 3x3 rotation matrix
         # Initialize c2w as look-at matrix to object center from random position
         self.register_parameter(name='camera_rot', param=torch.nn.Parameter(init_c2w[:3, :3]))
         self.register_parameter(name='camera_pos', param=torch.nn.Parameter(init_c2w[:3, -1]))
         self.to(device)
+
+        self.vis = [[], []]
+
 
     def forward(self, c2w=None, num_pixel_samples=800):
         """
@@ -50,13 +54,14 @@ class iNeRF(torch.nn.Module):
     @property
     def c2w(self):
         c2w = torch.zeros((4, 4)).to(self.device)
-        c2w[:3, :3] = torch.eig(self.camera_rot, eigenvectors=True)[1]
+        c2w[:3, :3] = self.camera_rot #torch.eig(self.camera_rot, eigenvectors=True)[1]
         c2w[:3, -1] = self.camera_pos
         c2w[-1, -1] = 1
         return c2w
 
     def fit(self, target_image):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-1)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-2, weight_decay=1e-4)
+        # lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.98)
         for i in range(2000):
             # Forward pass: Compute predicted y by passing x to the model
             pred_pixels, pixel_idxs = self.forward()
@@ -65,33 +70,42 @@ class iNeRF(torch.nn.Module):
             loss = ((pred_pixels - target_pixels)**2).sum()
 
             if i % 10 == 0:
-                # if i ==0:
-                #     vis_pixies(target_pixels, pixel_idxs, self.image_res)
-                # vis_pixies(pred_pixels, pixel_idxs, self.image_res)
-
+                current_img = self.render().detach().cpu()
+                self.vis[0].append((target_image.detach().cpu() + current_img * 2) / 3)
+                self.vis[1].append(vis_pixies(pred_pixels, pixel_idxs, self.image_res))
+            if i % 20 == 0:
+                if i == 0:
+                    vis_pixies(target_pixels, pixel_idxs, self.image_res)
                 print(i, loss.item())
-                fig = plt.figure()
-                plt.imshow(self.render().detach().cpu())
-                fig.suptitle(f"Pred image {i}")
-                plt.show()
+
+                # fig = plt.figure()
+                # plt.imshow(current_img)
+                # fig.suptitle(f"Pred image {i}")
+                # plt.show()
 
             # Zero gradients, perform a backward pass, and update the weights.
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            # lr_scheduler.step()
 
-        print(f'Result: {model.string()}')
+        self.vis[0] = torch.stack(self.vis[0], dim=0).permute(0, 3, 1, 2)
+        self.vis[1] = torch.stack(self.vis[1], dim=0).permute(0, 3, 1, 2)
+        video = ts.show_video(self.vis, display=True)
+        video.save("out.gif")
+        print(f'Result: {loss}')
 
 def vis_pixies(pred_pixels, pixel_idxs, wh=(800, 800), device='cuda'):
     img = torch.zeros((*wh, 3)).to(device)
     img[pixel_idxs[..., 0], pixel_idxs[..., 1]] = pred_pixels
     img = img.unsqueeze(0)
     img = img.permute(0, 3, 1, 2)
-    img = kornia.box_blur(img * wh[0]//50 * wh[1]//50, (wh[0]//50, wh[1]//50), 'constant', normalized=False)
-    fig = plt.figure()
-    plt.imshow(img[0].permute(1, 2, 0).detach().cpu())
-    fig.suptitle("Target image")
-    plt.show()
+    img = kornia.box_blur(img * wh[0]//30 * wh[1]//30, (wh[0]//30, wh[1]//30), 'constant', normalized=False)
+    # fig = plt.figure()
+    # plt.imshow(img[0].permute(1, 2, 0).detach().cpu())
+    # fig.suptitle("Target image")
+    # plt.show()
+    return img[0].permute(1, 2, 0).detach().cpu()
 
 if __name__ == "__main__":
     device = "cuda"

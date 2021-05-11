208fbfc019d5a0de1c0f72e9bcb9f63f880a7eb0
axquaris <dominykas@berkeley.edu>
2021-05-10 23:50:34 -0700

    euler init glitch

--- inerf/main.py
+++ inerf/main.py
@@ -13,7 +13,7 @@ from render_utils import NeRF_Renderer, SVOX_Renderer
 from transform_utils import twist_to_matrix
 
 class iNeRF(torch.nn.Module):
-    def __init__(self, pose_repr="twist", device="cuda"):
+    def __init__(self, pose_repr="twist", sampling="canny", device="cuda"):
         """
         In the constructor we instantiate four parameters and assign them as
         member parameters.
@@ -24,9 +24,10 @@ class iNeRF(torch.nn.Module):
         self.NeRF_renderer = NeRF_Renderer(self.image_res)
         self.svox_renderer = SVOX_Renderer(self.image_res)
         self.pose_repr = pose_repr
+        self.sampling = sampling
 
-        self.init_c2w = self.NeRF_renderer.dataset[0]["c2w"]
-        self.init_c2w = torch.vstack((self.init_c2w, torch.tensor([0., 0., 0., 1.])))
+        self.init_c2w = torch.vstack((self.NeRF_renderer.dataset[9]["c2w"],
+                                      torch.tensor([0., 0., 0., 1.]))).to(self.device)
         if pose_repr == "matrix":
             # Use euler angles, yaw and pitch as parameters of 3x3 rotation matrix
             # Initialize c2w as look-at matrix to object center from random position
@@ -40,7 +41,7 @@ class iNeRF(torch.nn.Module):
         self.vis = [[], []]
 
 
-    def forward(self, c2w=None, num_pixel_samples=800, target_image=None):
+    def forward(self, c2w=None, num_pixel_samples=100, target_image=None):
         """
         In the forward function we accept a Tensor of input data and we must return
         a Tensor of output data. We can use Modules defined in the constructor as
@@ -48,7 +49,12 @@ class iNeRF(torch.nn.Module):
         """
         c2w = self.c2w() if c2w is None else c2w.to(self.device)
         # print(c2w)
-        pixel_idxs = sample_pixels(num_pixel_samples, self.image_res).to(self.device)
+        if self.sampling == "random":
+            pixel_idxs = sample_pixels(num_pixel_samples, self.image_res).to(self.device)
+        elif self.sampling == "canny":
+            pixel_idxs = sample_pixels_edge_bias(num_pixel_samples, target_image, self.image_res).to(self.device)
+        else:
+            raise NotImplementedError(f"Invalid sample {self.sampling}")
         # pixel_idxs = sample_pixels_edge_bias(num_pixel_samples, target_image, self.image_res).to(self.device)
         rays = sample_rays(pixel_idxs, c2w, self.NeRF_renderer.dataset)
         return self.NeRF_renderer.render_rays(rays)[0], pixel_idxs
@@ -58,15 +64,27 @@ class iNeRF(torch.nn.Module):
         image_res = self.image_res if image_res is None else image_res
         return self.svox_renderer.render_image(c2w, image_res)[0]
 
-    def c2w(self):
+    def c2w(self, yaw=None, pitch=None):
         if self.pose_repr == "matrix":
             c2w = torch.zeros((4, 4)).to(self.device)
             c2w[:3, :3] = self.camera_rot
             c2w[:3, -1] = self.camera_pos
             c2w[-1, -1] = 1
         elif self.pose_repr == "twist":
-            c2w = twist_to_matrix(self.camera_twi, self.camera_rot, self.init_c2w.to(self.device))
+            c2w = twist_to_matrix(self.camera_twi, self.camera_rot) @ self.init_c2w
+        elif self.pose_repr == "euler":
+            if yaw is None:
+                yaw = self.camera_yaw
+            if pitch is None:
+                pitch = self.camera_pitch
 
+            w2w_rot = torch.eye(4, device=device)
+            w2w_rot[:3, :3] = yaw_pitch_to_rotation_matrix(yaw, pitch)[0]
+            c2c_dist =torch.eye(4, device=device)
+            c2c_dist[2, -1] = self.camera_dist
+            c2w =  w2w_rot @ self.init_c2w_ @ c2c_dist
+        else:
+            raise NotImplementedError(f"Invalid pose_repr {self.pose_repr}")
         return c2w
 
     """
@@ -100,14 +118,39 @@ class iNeRF(torch.nn.Module):
       - Could this be considered a pose refinement strategy? consider success rates for grid of (init distance X sample rate)
     """
     def fit(self, target_image):
+        # Initialization Prologue
+        if self.pose_repr == "euler":
+            with torch.no_grad():
+                n = 10
+                best_loss = float('inf')
+                best_yaw_pitch = None
+                for pitch in torch.arange(0, np.pi / 3,
+                                          1 / 3 * np.pi / 6):
+                    for yaw in torch.arange(-np.pi, np.pi,
+                                            2 * np.pi / 10):
+                        c2w = self.c2w(yaw, pitch)
+
+                        pred_pixels, pixel_idxs = self.forward(c2w, target_image=target_image.to(self.device))
+                        target_pixels = target_image[pixel_idxs[:, 0], pixel_idxs[:, 1]]
+                        loss = ((pred_pixels - target_pixels) ** 2).mean()
+
+                        if loss < best_loss:
+                            best_loss = loss
+                            best_yaw_pitch = (yaw, pitch)
+                print(best_yaw_pitch)
+                self.register_parameter(name='camera_yaw', param=torch.nn.Parameter(torch.tensor([best_yaw_pitch[0] + 1e5])))
+                self.register_parameter(name='camera_pitch', param=torch.nn.Parameter(torch.tensor([best_yaw_pitch[1] + 1e5])))
+                # self.register_parameter(name='camera_dist', param=torch.nn.Parameter(torch.tensor([1e-5])))
+
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-2, weight_decay=1e-4)
         # lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.98)
+
         for i in range(200):
             # Forward pass: Compute predicted y by passing x to the model
             pred_pixels, pixel_idxs = self.forward(target_image=target_image.to(self.device))
             target_pixels = target_image[pixel_idxs[:, 0], pixel_idxs[:, 1]]
 
-            loss = ((pred_pixels - target_pixels)**2).sum()
+            loss = ((pred_pixels - target_pixels)**2).mean()
 
             if i % 10 == 0:
                 current_img = self.render().detach().cpu()
@@ -133,7 +176,6 @@ class iNeRF(torch.nn.Module):
         self.vis[1] = torch.stack(self.vis[1], dim=0).permute(0, 3, 1, 2)
         video = ts.show_video(self.vis, display=True)
         video.save("out.gif")
-        print(f'Result: {loss}, xi: {self.camera_twi}, theta: {self.camera_rot}')
 
 def vis_pixies(pred_pixels, pixel_idxs, wh=(800, 800), device='cuda'):
     img = torch.zeros((*wh, 3)).to(device)
@@ -151,7 +193,7 @@ def vis_pixies(pred_pixels, pixel_idxs, wh=(800, 800), device='cuda'):
 if __name__ == "__main__":
     device = "cuda"
     with torch.no_grad():
-        model = iNeRF(pose_repr="twist")
+        model = iNeRF(pose_repr="euler", sampling="canny")
         model.to(device)
 
         # Matrix copied from lego test set image 0
--- inerf/transform_utils.py
+++ inerf/transform_utils.py
@@ -22,7 +22,7 @@ import matplotlib.pyplot as plt
 
 device="cuda"
 
-def twist_to_matrix(xi, theta, initial_c2w, device="cuda"):
+def twist_to_matrix(xi, theta, device="cuda"):
     vee, omega = xi[:3], xi[3:]
 
     omega_hat = torch.zeros((3, 3)).to(device)
@@ -39,36 +39,56 @@ def twist_to_matrix(xi, theta, initial_c2w, device="cuda"):
                                              + (torch.outer(omega, omega) @ vee) * theta)
     exp_xi[3, 3] = 1.0
 
-    return exp_xi @ initial_c2w
+    return exp_xi
 
-# def pitch_yaw_to_rotation_matrix(pitch, yaw):
-#     dir = torch.tensor([1, 0, 0]).to(device)
-#     pitch = kornia.geometry.angle_axis_to_rotation_matrix(pitch.to(device) * dir.permute())
-#     yaw = kornia.geometry.angle_axis_to_rotation_matrix(yaw.to(device) * dir)
-#     return yaw @ pitch # apply yaw then pitch when going from camera->world->perturbed_world
+def yaw_pitch_to_rotation_matrix(yaw, pitch, device="cuda"):
+    up = torch.tensor([[0, 0, 1]]).to(device)
+    yaw_m = kornia.geometry.angle_axis_to_rotation_matrix(yaw.to(device) * up)
+
+    right = torch.tensor([[-1, 0, 0]]).to(device)
+    pitch_m = kornia.geometry.angle_axis_to_rotation_matrix(pitch.to(device) * right)
+    return yaw_m @ pitch_m # apply yaw then pitch when going from camera->world->perturbed_world
 
 
 # def matrix_to_pitch_yaw(pitch, yaw):
 #     pass
 
-# if __name__ == "__main__":
-#     device = "cuda"
-#
-#     image_res = (400, 400)
-#     render_svox = SVOX_Renderer(image_res)
-#     image_list = []
-#
-#     with torch.no_grad():
-#         # Matrix copied from lego test set image 0
-#         c2w = torch.tensor([[-0.9999999403953552, 0.0, 0.0, 0.0],
-#                             [0.0, -0.7341099977493286, 0.6790305972099304, 2.737260103225708],
-#                             [0.0, 0.6790306568145752, 0.7341098785400391, 2.959291696548462],
-#                             [0.0, 0.0, 0.0, 1.0],
-#                             ], device=device)
-#         rot = pitch_yaw_to_rotation_matrix(torch.Tensor([0]), torch.Tensor([0]))
-#         svox_image, _ = render_svox.render_image(c2w @ rot)
-#         image_list.append(svox_image)
-#
-#     image_list = torch.stack(image_list, dim=0).permute(0, 3, 1, 2)
-#     video = ts.show_video(image_list, display=True)
-#     video.save("360.gif")
+if __name__ == "__main__":
+    device = "cuda"
+
+    image_res = (400, 400)
+    render_svox = SVOX_Renderer(image_res)
+    image_list = []
+
+    with torch.no_grad():
+        n = 10
+        for pitch in torch.arange(0, np.pi / 3,
+                                  1 / 3 * np.pi / 6):
+            for yaw in torch.arange(-np.pi, np.pi,
+                                    2 * np.pi / 10):
+                # Matrix copied from lego test set image 0
+                # c2w_init = torch.tensor([[-0.9999999403953552, 0.0, 0.0, 0.0],
+                #                          [0.0, -0.7341099977493286, 0.6790305972099304, 2.737260103225708],
+                #                          [0.0, 0.6790306568145752, 0.7341098785400391, 2.959291696548462],
+                #                          [0.0, 0.0, 0.0, 1.0],
+                #                          ], device=device)
+                c2w_init = torch.tensor([[1., 0., 0., 0.],
+                                         [0., 1., 0., .3],
+                                         [0., 0., 1., 3.3 + 5],
+                                         [0., 0., 0., 1.],
+                                         ], device=device)
+                init_correction = torch.eye(4, device=device)
+                init_correction[:3, :3] = yaw_pitch_to_rotation_matrix(torch.Tensor([0]), torch.Tensor([-np.pi/2]))[0]
+                w2w_rot = torch.eye(4, device=device)
+                w2w_rot[:3, :3] = yaw_pitch_to_rotation_matrix(yaw, pitch)[0]
+
+                svox_image, _ = render_svox.render_image(w2w_rot @ init_correction @ c2w_init)
+
+                # plt.imshow(svox_image.cpu())
+                # plt.show()
+                # exit()
+                image_list.append(svox_image)
+
+    image_list = torch.stack(image_list, dim=0).permute(0, 3, 1, 2)
+    video = ts.show_video(image_list, display=True)
+    video.save("360.gif")
